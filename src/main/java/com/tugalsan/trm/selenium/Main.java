c0b89f020754885c2a37c3a739137e4994e7b604
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-05 16:07:35 +0300

    Update Main.java

--- src/main/java/com/tugalsan/trm/selenium/Main.java
+++ src/main/java/com/tugalsan/trm/selenium/Main.java
@@ -16,11 +16,12 @@ public class Main {
     //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.selenium
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.trm.selenium-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String[] args) {
-//        EdgeOptions options = new EdgeOptions();
-//options.setBinary("C:\\Program Files (x86)\\Microsoft\\Edge Beta\\Application\\");
-//EdgeDriver driver = new EdgeDriver(options);
-        
-        
+        /*
+            Pre-request:
+            Microsoft Edge Web Sürücüsü | Microsoft Edge Geliştiricisi: https://developer.microsoft.com/tr-tr/microsoft-edge/tools/webdriver/?form=MA13LH#downloads
+            Kararlı Kanal – x64 -> Download to: C:\\bin\\selenium\\edgedriver_win64\\msedgedriver.exe
+         */
+
         System.setProperty("webdriver.edge.driver", "C:\\bin\\selenium\\edgedriver_win64\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
@@ -35,37 +36,3 @@ public class Main {
         System.out.println("expectedUrl: " + expectedUrl);
     }
 }
-
-/*
-
-
-Pre-request:
-Microsoft Edge Web Sürücüsü | Microsoft Edge Geliştiricisi
-Kararlı Kanal – x64
-Download to: C:\\bin\\selenium\\edgedriver_win64\\msedgedriver.exe
-
-Pom.xml:
-<dependency>
-	<groupId>org.seleniumhq.selenium</groupId>
-	<artifactId>selenium-edge-driver</artifactId>
-	<version>4.21.0</version>
-</dependency>
-
-module-info.java:
-org.seleniumhq.selenium.edge_driver;
-
-Main.java:
-System.setProperty("webdriver.edge.driver", "C:\\bin\\selenium\\edgedriver_win64\\msedgedriver.exe");
-WebDriver driver = new EdgeDriver();
-driver.manage().window().maximize();
-driver.get("https://www.browserstack.com/users/sign_in");
-WebElement username = driver.findElement(By.id("user_email_login"));
-WebElement password = driver.findElement(By.id("user_password"));
-WebElement login = driver.findElement(By.name("commit"));
-username.sendKeys("abc@gmail.com");
-password.sendKeys("your_password");
-login.click();
-String expectedUrl = driver.getCurrentUrl();
-System.out.println("expectedUrl: " + expectedUrl);
-
-*/
